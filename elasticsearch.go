d8ec05d404d160d4df61d050a53eda8d4da97c8d
pipiBRH <a8843526@gmail.com>
2019-06-29 22:55:01 +0800

    first

---
+++ elasticsearch.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/olivere/elastic"
+)
+
+type ElasticsearchConnection struct {
+	Ctx    context.Context
+	Client *elastic.Client
+}
+
+var EsClient *ElasticsearchConnection
+
+func InitElasticsearchConnection(host string, port int) error {
+	ctx := context.Background()
+	errorlog := log.New(os.Stdout, "APP", log.LstdFlags)
+	addr := fmt.Sprintf("http://%s:%d", host, port)
+
+	client, err := elastic.NewClient(
+		elastic.SetErrorLog(errorlog),
+		elastic.SetURL(addr),
+		// elastic.SetHealthcheck(false),
+		elastic.SetSniff(false),
+	)
+	if err != nil {
+		return err
+	}
+
+	_, _, err = client.Ping(addr).Do(ctx)
+	if err != nil {
+		return err
+	}
+
+	EsClient = &ElasticsearchConnection{
+		Ctx:    ctx,
+		Client: client,
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/pipiBRH/kk_database
+
+go 1.12
+
+require (
+	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
+	github.com/olivere/elastic v6.2.19+incompatible
+	github.com/pkg/errors v0.8.1 // indirect
+)
